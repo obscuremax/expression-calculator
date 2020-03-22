1983ca763833cb3d422004c49d53c9a518264704
obscuremax <maxxam@yandex.ru>
2020-03-22 18:41:50 +0300

    resolve task expression-calculator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let count = 0
+    for (let i = 0; i < expr.length; i++){
+        if (expr[i] == '(') count++
+        if (expr[i] == ')') count--
+    }
+    if (count != 0 ) throw "ExpressionError: Brackets must be paired"
+    expr = expr.replace(/\s/g,"")
+    if (expr.indexOf('/0') != -1) throw "TypeError: Division by zero."
+    let exprNew = new Function(`return ${expr}`)
+    return exprNew()
 }
 
 module.exports = {
